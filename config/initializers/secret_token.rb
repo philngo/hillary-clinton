7e2b4dcec477d96b4eefcfd8cd91a9e065d33e8c
Philip Ngo <ngo12@college.harvard.edu>
2013-04-27 17:53:43 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HillaryClinton::Application.config.secret_token = '6efe1fc2809c9e218cf8642890a0fa60ec97bb51191adbb1307fd949bbfed5a1b812e5bc9f01b1f4436f581ba3f1e942051b713b84ebb12ff5fcc00b8c563434'
